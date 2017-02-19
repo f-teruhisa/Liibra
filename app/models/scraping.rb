901f742c1ae6a8b64674cc6034ce9f0a39dc3720
福本　晃之 <teru@TERU-on-MacBook.local>
2017-02-19 14:05:00 +0900

    ・Add Scraping Data(Prefecturea) ・header delete from User Authenticate

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,11 +1,31 @@
-require "sparql/client"
+class Scraping
 
-client = SPARQL::Client.new("http://ja.dbpedia.org/sparql")
-results = client.query("select distinct ?name ?abstract where {
-    ?company <http://dbpedia.org/ontology/wikiPageWikiLink> <http://ja.dbpedia.org/resource/Category:東証一部上場企業> .
-    ?company rdfs:label ?name .
-    ?company <http://dbpedia.org/ontology/abstract> ?abstract .}")
+require "nokogiri"
+require "open-uri"
 
-results.each do |solution|
-  puts "#{solution[:resource]} | #{solution[:title]}"
+@base_url = "https://ja.wikipedia.org"
+
+# 画像の検索（県のロゴを検索）
+def self.image_serch(url)
+  doc = Nokogiri::HTML(open(@base_url+url))
+  return doc.xpath("//a[@class = 'image']/img").attribute("src").value
+end
+
+def self.category_serch
+  # 日本の都道府県カテゴリURL
+  url = "/wiki/Category:%E6%97%A5%E6%9C%AC%E3%81%AE%E9%83%BD%E9%81%93%E5%BA%9C%E7%9C%8C"
+  doc = Nokogiri::HTML(open(@base_url+url))
+  doc.xpath("//div[@class = 'CategoryTreeItem']/a").each do |element|
+  # 関係ないページを省く(県名の長さで排除)
+  if element.text.length <= 4
+    name = element.text
+    image_url = image_serch(element[:href])
+    link = element[:href]
+    elements = Element.new(name: name, image_url: image_url, link: link , score: 1500)
+    elements.save
+  end
+  end
+end
+
+category_serch
 end
