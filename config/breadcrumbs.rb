2491f22d4503d7818eefc44834ba063ecba5cc60
福本　晃之 <teru@TERU-on-MacBook.local>
2017-09-09 22:21:51 +0900

    some file comit

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,48 @@
+# Top
+crumb :root do
+  link "Home", root_path
+end
+
+# Genre
+crumb :genre do
+  link "Genre",genres_path
+  parent :root
+end
+
+# User
+crumb :user do
+  link "My Page"
+  parent :root
+end
+
+crumb :user_edit do
+  link "User Edit"
+  parent :root
+end
+
+# Theme
+crumb :theme_index do
+  link "Theme",genre_themes_path
+  parent :genre
+end
+
+crumb :theme_choose do
+  link "Choose","/genres/#{params[:genre_id]}/themes/show"
+  parent :theme_index
+end
+
+crumb :theme_registration do
+  link "Registration", new_genre_theme_path
+  parent :theme_index
+end
+
+crumb :theme_search do
+  link "Search", search_genre_themes_path
+  parent :theme_index
+end
+
+# Rate
+crumb :rate do
+  link "Rate",genre_theme_rates_path
+  parent :theme_choose
+end
